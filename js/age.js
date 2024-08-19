7bdd1ad902f8a49e7de75a172a69820d6d17a3b7
DEXV <129897824+Dexiikk@users.noreply.github.com>
2024-08-19 16:54:56 +0200

    Delete js directory

--- js/age.js
+++
@@ -1,6 +0,0 @@
-let ageEl = document.getElementById("age");
-
-setInterval(() => {
-	let time = dayjs().diff(dayjs(1167609600000), 'year', true);
-	ageEl.innerText = time.toString().substring(0, 12);
-}, 50);
