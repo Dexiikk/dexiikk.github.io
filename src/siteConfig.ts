a96edd0c7d7641b94c6d759908d6bc0e9e5a150e
DEXV <129897824+Dexiikk@users.noreply.github.com>
2025-01-27 23:26:15 +0100

    Add files via upload

---
+++ src/siteConfig.ts
@@ -0,0 +1,28 @@
+import siteConfigJson from "./siteConfig.json";
+
+export interface SiteConfiguration {
+  name: string;
+  bio: string;
+  profilePicture: string;
+  url: string;
+  blog: boolean;
+  iconLinks: IconLink[];
+  customLinks: CustomLink[];
+}
+
+interface IconLink {
+  id: string;
+  icon: string;
+  url: string;
+}
+
+interface CustomLink {
+  id: string;
+  title: string;
+  url: string;
+  icon: string
+}
+
+export const SITE: SiteConfiguration = {
+  ...siteConfigJson,
+};
